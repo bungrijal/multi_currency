081f4b99dd0eae1badddc48a1d6dd9982e99d2c8
Rijaludin Muhsin <rijaludinmuhsin@gmail.com>
2014-06-17 14:04:35 +0700

    Ability to specify date when specifying amount in other currency

--- lib/multi_currency.rb
+++ lib/multi_currency.rb
@@ -21,17 +21,17 @@ module MultiCurrency
     def multi_currency_for(columns)
       multi_currency_columns = columns
       multi_currency_columns.each do |column|
-        define_singleton_method "sum_#{column}" do |currency|
+        define_singleton_method "sum_#{column}" do |currency, date = Date.today|
+          formatted_date = date.strftime("%Y-%m-%d")
           self.sum("
             CASE #{column}_currency
               WHEN '#{currency.downcase}' THEN #{column}
-              ELSE #{column} * (SELECT exchange_rates.rate FROM exchange_rates WHERE (exchange_rates.from_code = #{column}_currency AND to_code = '#{currency.downcase}' AND date = #{column}_rate_date) )
+              ELSE #{column} * (SELECT exchange_rates.rate FROM exchange_rates WHERE (exchange_rates.from_code = #{column}_currency AND to_code = '#{currency.downcase}' AND date = '#{formatted_date}') )
             END")
         end
 
-        define_method "#{column}_in" do |currency_code|
+        define_method "#{column}_in" do |currency_code, date = Date.today|
           default_currency = self.send("#{column}_currency") rescue MultiCurrency.configuration.default_currency
-          date = self.send("#{column}_rate_date") rescue Date.today
           rate = MultiCurrency.configuration.default_converter.get_rate_and_cache(default_currency, currency_code, date)
           self.send(column) * rate
         end
